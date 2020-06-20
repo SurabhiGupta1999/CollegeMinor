6bdd2f2345576d290a93a680ba745a7027c2838d
SurabhiGupta1999 <guptasurabhi486@gmail.com>
2020-06-20 13:28:19 +0530

    student dashboard

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, ses
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from flask_bcrypt import Bcrypt
-from forms import SignUpForm, LoginForm, ForgotForm
+from forms import SignUpForm, LoginForm, ForgotForm, ComplaintForm
 
 
 engine = create_engine("postgres://liscgjktgvoiid:e44ad379dfcc1fce6ed0f3de28c59077c899252c7fe094565383de810e598d80@ec2-52-86-73-86.compute-1.amazonaws.com:5432/d2uiio9chcha1n")
@@ -18,6 +18,14 @@ app.config['SECRET_KEY'] = '123minor456project789'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
+    a_email = session.get('aemail')
+    if a_email:
+        return redirect(url_for('admin'))
+
+    stud_email = session.get('semail')
+    if stud_email:
+        return redirect(url_for('student'))
 
     form = SignUpForm()
 
@@ -34,15 +42,15 @@ def index():
 
 
         if info:
-            flash('This Email ID already registered with us.')
+            flash('This Email ID already registered with us. Please use another email id to register.')
             return redirect(url_for('index'))
-        else:
+        elif design == "admin":
             db.execute("INSERT INTO admininfo (id, fname, email, password, contactno, designation) VALUES (:id, :fname, :email, :password, :contactno, :designation)", {'id' : id, 'fname' : fname, 'email' : email, 'password' : hash_password, 'contactno' : contactno, 'designation' : design})
             db.commit()
-        if info_one:
-            flash('This Email ID already registered with us.')
+        elif info_one:
+            flash('This Email ID already registered with us. Please use another email id to register.')
             return redirect(url_for('index'))
-        else:
+        elif design == "stud":
             db.execute("INSERT INTO studentinfo (id, fname, email, password, contactno, designation) VALUES (:id, :fname, :email, :password, :contactno, :designation)", {'id' : id, 'fname' : fname, 'email' : email, 'password' : hash_password, 'contactno' : contactno, 'designation' : design})
             db.commit()
 
@@ -68,15 +76,18 @@ def login():
         info_one = db.execute("SELECT * FROM studentinfo WHERE email = :email", {"email": email }).fetchone()
 
 
+
         if design == "admin":
-            if info and bcrypt.check_password_hash(info.password, password):
+            if info and bcrypt.check_password_hash(info.password, password) is True:
+                session['aemail'] = info.email
                 return redirect(url_for('admin'))
             else:
                 flash('You have entered incorrect Email ID or Password, please enter correct Email ID and Password')
                 return redirect(url_for('login'))
         if design == "stud":
-            if info_one and bcrypt.check_password_hash(info.password, password):
-                return redirect(url_for('admin'))
+            if info_one and bcrypt.check_password_hash(info_one.password, password) is True:
+                session['semail'] = info_one.email
+                return redirect(url_for('student'))
             else:
                 flash('You have entered incorrect Email ID or Password, please enter correct Email ID and Password')
                 return redirect(url_for('login'))
@@ -84,6 +95,16 @@ def login():
 
 
     return render_template('login.html', form=form)
+
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('semail', None)
+    session.pop('aemail', None)
+
+
+    return redirect(url_for('index'))
 
 
 @app.route('/forgot', methods=['GET', 'POST'])
@@ -102,3 +123,51 @@ def forgot():
 def admin():
 
     return render_template('admin.html')
+
+
+
+@app.route('/student', methods=['GET', 'POST'])
+def student():
+
+    stud_email = session.get('semail')
+
+    form = ComplaintForm()
+
+    if form.validate_on_submit():
+        fname = form.fname.data
+        email = form.email.data
+        date = form.date.data
+        degree = form.degree.data
+        semester = form.semester.data
+        complaintcategory = form.complaintcategory.data
+        message = form.message.data
+        #file = form.file.data
+
+        db.execute("INSERT INTO complain (fname, email, date, degree, semester, complaintcategory, message) VALUES (:fname, :email, :date, :degree, :semester, :complaintcategory, :message)", {'fname' : fname, 'email' : email, 'date' : date, 'degree' : degree, 'semester' : semester, 'complaintcategory' : complaintcategory, 'message' : message})
+        db.commit()
+
+        flash('Complaint Registered Successfully.')
+
+        return redirect(url_for('student'))
+
+
+    return render_template('student.html', form=form)
+
+
+@app.route('/complainthistory', methods=['GET', 'POST'])
+def complainthistory():
+
+    stud_email = session.get('semail')
+
+    complaintdetail = db.execute("SELECT * FROM complain WHERE email = :email", {'email' : stud_email}).fetchall()
+
+
+    return render_template('complainthistory.html', complaintdetail=complaintdetail)
+
+
+@app.route('/studprofileupdate', methods=['GET', 'POST'])
+def studprofileupdate():
+
+    stud_email = session.get('semail')
+
+    studinfo = db.execute("SELECT * FROM studentinfo WHERE email = :email", {'email' : stud_email}).fetchone()
--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField,SubmitField, PasswordField, RadioField, SubmitField
-from wtforms.fields.html5 import EmailField, TelField
+from wtforms import StringField, SelectField,SubmitField, PasswordField, RadioField, MultipleFileField, SubmitField, TextAreaField
+from wtforms.fields.html5 import EmailField, TelField, DateField
 from wtforms.validators import DataRequired, Email, Length, InputRequired
 
 class SignUpForm(FlaskForm):
     id = StringField('ID*', validators=[DataRequired()])
     fname = StringField('Full Name*', validators=[DataRequired()])
-    email = EmailField('Email ID*',validators=[DataRequired(), Email()])
+    email = EmailField('Email Id*',validators=[DataRequired(), Email()])
     password = PasswordField('Password*', validators=[DataRequired()])
     contactno = TelField('Mobile No*.', validators=[DataRequired(), Length(min=10, max=10)])
     design = SelectField(u'Designation*', choices=[('admin', 'Admin'), ('stud', 'Student')], validators=[DataRequired()])
@@ -14,13 +14,26 @@ class SignUpForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('Email ID*',validators=[DataRequired(), Email()])
+    email = EmailField('Email Id*',validators=[DataRequired(), Email()])
     password = PasswordField('Password*', validators=[DataRequired()])
     design = SelectField(u'Designation*', choices=[('admin', 'Admin'), ('stud', 'Student')], validators=[DataRequired()])
     submit = SubmitField('Login >>')
 
 
 class ForgotForm(FlaskForm):
-    email = EmailField('Email ID*',validators=[DataRequired(), Email()])
+    email = EmailField('Email Id*',validators=[DataRequired(), Email()])
     design = SelectField(u'Designation*', choices=[('admin', 'Admin'), ('stud', 'Student')], validators=[DataRequired()])
     submit = SubmitField('Change your Password')
+
+
+
+class ComplaintForm(FlaskForm):
+    fname = StringField('Full Name *', validators=[DataRequired()])
+    email = EmailField('Email Id*',validators=[DataRequired(), Email()])
+    date = DateField('Date', validators=[DataRequired()])
+    degree = SelectField(u'Degree*', choices=[('bachelors', 'Bachelors'), ('masters', 'Masters')], validators=[DataRequired()])
+    semester = SelectField(u'Semester*', choices=[('first', 'First'), ('second', 'Second'), ('third', 'Third'), ('fourth', 'Fourth'), ('fifth', 'Fifth'), ('sixth', 'Sixth'), ('seventh', 'Seventh'), ('eighth', 'Eighth')], validators=[DataRequired()])
+    complaintcategory = SelectField(u'Complant Category*', choices=[('infrastructure', 'Infrastructure'), ('accounts', 'Accounts'), ('academics', 'Academics'), ('management', 'Management'), ('faculty', 'Faculty'), ('library', 'Library')], validators=[DataRequired()])
+    message = TextAreaField('Enter Complaint Details', validators=[DataRequired(), Length(max=100)])
+    #file = MultipleFileField(u'Upload File')
+    submit = SubmitField('Submit')
